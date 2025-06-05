586a90c0ec89437e23fba78aff9ebb16160fd5ad
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-06-05 12:32:59 +0200

    Update StampaSaluto.java to print greetings and add .gitignore for .DS_Store

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,8 +1,8 @@
 package org.lessons.java;
 
 public class StampaSaluto {
-    public static void main(String[] args) {
-    System.out.println("Weeeee ciao Java!");
-    System.out.println("Hello Java World!");
+    public static void main (String[] args) {
+    System.out.print("Hello Java!\n");
+    System.out.println("Hello de novo");
     }
 }
